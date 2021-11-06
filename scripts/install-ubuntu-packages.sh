57a5b92692b0dd01cdb8a598a07f4d533a1ec251
Alexander Fast <139879+alexanderfast@users.noreply.github.com>
2021-11-06 12:01:23 +0100

    set -xe

--- scripts/install-ubuntu-packages.sh
+++ scripts/install-ubuntu-packages.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -xe
-sudo apt-get install -y git zsh tmux python3 fonts-powerline
+#!/bin/bash
 
 # ripgrep isnt in apt before v20
 major=$(lsb_release -r | grep Release | grep -oP '\d+\.\d+' | cut -d'.' -f1)
+set -xe
 if [ "$major" -gt 18 ]; then
     sudo apt install -y ripgrep
 else
@@ -10,3 +10,5 @@ else
     curl -LO https://github.com/BurntSushi/ripgrep/releases/download/$version/ripgrep_${version}_amd64.deb
     sudo dpkg -i ripgrep_${version}_amd64.deb
 fi
+
+sudo apt-get install -y git zsh tmux python3 fonts-powerline
