e91869befbbeee7d2c05a642fdf1fbdfc56466f0
Alexander Fast <git@alexanderfast.se>
2023-02-08 09:05:21 +0100

    New indent char more common in fonts

--- nvim/init.lua
+++ nvim/init.lua
@@ -209,7 +209,7 @@ require('Comment').setup()
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
 require('indent_blankline').setup {
-  char = '┊',
+  char = '¦',
   show_trailing_blankline_indent = false,
 }
 
