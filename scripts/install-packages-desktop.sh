aaff70efd50c270501cb6f5ec2c624b47bce2a77
Alexander Fast <git@alexanderfast.se>
2023-04-23 07:50:43 +0200

    Split desktop packages from regular

---
+++ scripts/install-packages-desktop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$(grep 'Arch Linux' /etc/os-release)" ]
+then
+else
+	# extra packages only for deskop
+	sudo pacman -Sq --noconfirm picom
+
+	# greeter
+	sudo pacman -Sq --noconfirm lightdm lightdm-gtk-greeter
+	sudo systemctl enable lightdm.service
+	sudo cp -f ~/.dotfiles/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+	sudo mkdir -p /etc/share/pixmaps
+	sudo cp ~/.dotfiles/wallpapers/cubes.png /etc/share/pixmaps/cubes.png
+
+	# enable ntp, why isnt this default?
+	timedatectl set-ntp true
+fi
--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -16,14 +16,7 @@ then
 
 	sudo apt-get install -y git zsh tmux python3 fonts-powerline cmake make fuse build-essential
 else
-	sudo pacman -Sq --noconfirm git zsh tmux python3 cmake make base-devel openssh bitwarden wget curl fuse unzip neovim ripgrep picom pyright npm ttf-hack-nerd
-
-	# greeter
-	sudo pacman -Sq --noconfirm lightdm lightdm-gtk-greeter
-	sudo systemctl enable lightdm.service
-	sudo cp -f ~/.dotfiles/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-	sudo mkdir -p /etc/share/pixmaps
-	sudo cp ~/.dotfiles/wallpapers/cubes.png /etc/share/pixmaps/cubes.png
+	sudo pacman -Sq --noconfirm git zsh tmux python3 cmake make base-devel openssh bitwarden wget curl unzip neovim ripgrep pyright npm ttf-hack-nerd
 
 	# enable ntp, why isnt this default?
 	timedatectl set-ntp true
