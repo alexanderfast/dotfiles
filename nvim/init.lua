8dc447919b676cfacbccf14db0a35cf6a51b8b46
Alexander Fast <git@alexanderfast.se>
2023-06-21 14:41:14 +0200

    Remove ruby, change fontsize, alacritty default terminal

--- nvim/init.lua
+++ nvim/init.lua
@@ -553,7 +553,7 @@ vim.keymap.set('n', '<leader>st', ":TodoTelescope<CR>", { desc = '[S]earch [T]od
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'toml', 'typescript', 'vimdoc', 'vim', 'kotlin', 'ruby' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'toml', 'typescript', 'vimdoc', 'vim', 'kotlin', },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
