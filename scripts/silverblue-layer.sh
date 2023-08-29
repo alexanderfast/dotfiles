197ec590976d7b34fcce10c799b0ae772121889d
Alexander Fast <git@alexanderfast.se>
2023-08-29 16:21:03 +0200

    Added ripgrep

--- scripts/silverblue-layer.sh
+++ scripts/silverblue-layer.sh
@@ -5,8 +5,8 @@
 # 'gcc-c++' autoconf automake binutils bison flex gcc gdb glibc-devel libtool make
 #
 # These are the dev environment, zsh tmux and nvim (TODO move these to distrobox)
-# fzf neovim npm python3-pygments tmux util-linux-user vim zsh
+# fzf neovim ripgrep npm python3-pygments tmux util-linux-user vim zsh
 #
 # These are extra:
 # distrobox kitty
-rpm-ostree install 'gcc-c++' autoconf automake binutils bison distrobox flex fzf gcc gdb glibc-devel kitty langpacks-en libtool make neovim npm python3-pygments tmux util-linux-user vim zsh
+rpm-ostree install 'gcc-c++' autoconf automake binutils bison distrobox flex fzf gcc gdb glibc-devel kitty langpacks-en libtool make neovim npm python3-pygments ripgrep tmux util-linux-user vim zsh
