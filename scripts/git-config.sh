11a4803d9e47aa8edb20073d73f4c55311ab67dc
Alexander Fast <git@alexanderfast.se>
2023-08-31 19:30:52 +0200

    Git config in dotfiles

---
+++ scripts/git-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# TODO find a better way to keep this git configs in dotfiles
+git config --global user.name "Alexander Fast"
+git config --global merge.tool nvimdiff  
+git config --global merge.conflictstyle diff3
