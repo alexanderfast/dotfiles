24b4702713acc94995826c1e248c486a0c497f37
Alexander Fast <git@alexanderfast.se>
2023-09-16 10:43:32 +0200

    Terminal tweaks and nerd fonts

--- scripts/install-font.sh
+++ scripts/install-font.sh
@@ -2,6 +2,6 @@
 
 fontdir=~/.local/share/fonts
 mkdir -p $fontdir
-wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip -O $fontdir/hack.zip
+wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip -O $fontdir/hack.zip
 unzip -o $fontdir/hack.zip -d $fontdir
 fc-cache $fontdir
---
+++ scripts/test-nerdfont.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo ""
+echo " " # note space after glyph, make it square if font works
