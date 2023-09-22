f435529f6250f3fe9a74ff1fab4ae1787ba32427
Alexander Fast <git@alexanderfast.se>
2023-09-22 15:35:05 +0200

    i3 nord again

--- scripts/install-pyenv.sh
+++ scripts/install-pyenv.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# Check prereqs https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+sudo apt update
+sudo apt install build-essential libedit-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+curl https://pyenv.run | bash
+pyenv install 3.9.6
