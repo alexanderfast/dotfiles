826458611841f4cf0b1048ff4e4da87eb1396425
Alexander Fast <git@alexanderfast.se>
2024-01-31 12:22:28 +0100

    Gruvbox nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,7 +8,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "gruvbox",
     },
   },
 }
--- nvim/lua/plugins/style.lua
+++ nvim/lua/plugins/style.lua
@@ -1,15 +1,17 @@
 return {
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = {}},
+
   { "EdenEast/nightfox.nvim" },
 
   {
     "folke/tokyonight.nvim",
-    lazy = true,
+    -- lazy = true,
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
-      style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
       light_style = "day", -- The theme is used when the background is set to light
-      transparent = true, -- Enable this to disable setting the background color
+      transparent = false, -- Enable this to disable setting the background color
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
       styles = {
         -- Style to be applied to different syntax groups
