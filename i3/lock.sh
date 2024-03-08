fa13f3a62778d353780a45d2f37174d4c764cfc4
Alexander Fast <git@alexanderfast.se>
2024-03-08 12:04:20 +0100

    Fix lockscreen

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,6 +1,5 @@
 img=/tmp/i3lock.png
 
-# scrot -o $img
-# convert $img -scale 10% -scale 1000% $img
-# i3lock -i $img
-i3lock -c 0000FF
+scrot -o $img
+convert $img -scale 10% -scale 1000% $img
+i3lock -i $img
